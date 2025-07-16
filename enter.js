b5240c69b9363461c6477d543c582ab335eadb19
JelacicBan <pomoci.kali@gmail.com>
2025-07-16 17:56:36 +0200

    Set iframe src on Enter

--- enter.js
+++ enter.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   enterBtn.addEventListener("click", () => {
     container.style.display = "none";
-    entryFrame.src = "/entrypage.html";// set iframe source
+    entryFrame.src = "entrypage.html";// set iframe source
     entryFrame.style.display = "block";
 
     if (window.startMusicAfterEnter) {
